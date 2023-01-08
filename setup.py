ba97b8243f5fdaa17559e15c8acd0dc7c7c0a442
intrapus <fturan.zero@gmail.com>
2023-01-09 01:43:59 +0300

    test

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='jango',
+    name='pyprivpackage',
     author='intrapus',
     author_email='dev@intrapus.com',
     description='Dependency Confusion Test',
